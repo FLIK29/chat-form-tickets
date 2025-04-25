b4906c07101c80c8445b872b32b95d22a7e0f603
Alex Elizalde <elizaldealexuploads@gmail.com>
2025-04-25 02:32:21 -0700

    Version completa con sala de espera y tickets

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAYP9aDqxl03zlDMF0A0sdpu9MFnX-mgI",
+  authDomain: "chat-form-app.firebaseapp.com",
+  projectId: "chat-form-app",
+  storageBucket: "chat-form-app.firebasestorage.app",
+  messagingSenderId: "1088172343766",
+  appId: "1:1088172343766:web:7883027f799a6db2e4e477"
+};
