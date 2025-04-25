869ffb0b10ad93eaa0d92f5f2fa2f32a483a04ab
FLIK29 <elizaldealexuploads@gmail.com>
2025-04-25 02:56:35 -0700

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// to be replaced
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
